c8b9c330ce555b43d7afa85591f047e8cc1bee31
jerrywebman <jerrycifeanyi@gmail.com>
2019-08-08 12:04:52 +0100

    still fixing build

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { disableReactDevTools } from '@fvilers/disable-react-devtools';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { disableReactDevTools } from '@fvilers/disable-react-devtools';
 
 if (process.env.NODE_ENV === 'production') {
     disableReactDevTools();
-  }
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
